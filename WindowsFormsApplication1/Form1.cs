a561271927e40faac1dc4aaa42ca8da6634a6bc0
SangNguyen <sangnguyen12cb7@gmail.com>
2021-03-24 14:28:34 +0700

    tao lable username

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
