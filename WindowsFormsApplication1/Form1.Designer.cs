310c793bf51fab4b6af921c9273d75f936304cb6
SangNguyen <sangnguyen12cb7@gmail.com>
2021-03-24 15:12:34 +0700

    tao button login

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,65 +33,76 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lbpassword = new System.Windows.Forms.Label();
             this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbUserName
             // 
             this.lbUserName.AutoSize = true;
-            this.lbUserName.Location = new System.Drawing.Point(69, 125);
-            this.lbUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbUserName.Location = new System.Drawing.Point(92, 154);
             this.lbUserName.Name = "lbUserName";
-            this.lbUserName.Size = new System.Drawing.Size(57, 13);
+            this.lbUserName.Size = new System.Drawing.Size(75, 17);
             this.lbUserName.TabIndex = 0;
             this.lbUserName.Text = "UserName";
             // 
             // txtuser
             // 
-            this.txtuser.Location = new System.Drawing.Point(146, 125);
-            this.txtuser.Margin = new System.Windows.Forms.Padding(2);
+            this.txtuser.Location = new System.Drawing.Point(195, 154);
+            this.txtuser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtuser.Name = "txtuser";
-            this.txtuser.Size = new System.Drawing.Size(186, 20);
+            this.txtuser.Size = new System.Drawing.Size(247, 22);
             this.txtuser.TabIndex = 1;
             this.txtuser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(201, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(268, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.Size = new System.Drawing.Size(78, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Đăng nhập";
             // 
             // lbpassword
             // 
             this.lbpassword.AutoSize = true;
-            this.lbpassword.Location = new System.Drawing.Point(72, 170);
+            this.lbpassword.Location = new System.Drawing.Point(96, 209);
+            this.lbpassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbpassword.Name = "lbpassword";
-            this.lbpassword.Size = new System.Drawing.Size(53, 13);
+            this.lbpassword.Size = new System.Drawing.Size(69, 17);
             this.lbpassword.TabIndex = 3;
             this.lbpassword.Text = "Password";
             this.lbpassword.Click += new System.EventHandler(this.label3_Click);
             // 
             // txtpassword
             // 
-            this.txtpassword.Location = new System.Drawing.Point(146, 162);
+            this.txtpassword.Location = new System.Drawing.Point(195, 199);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtpassword.Name = "txtpassword";
-            this.txtpassword.Size = new System.Drawing.Size(186, 20);
+            this.txtpassword.Size = new System.Drawing.Size(247, 22);
             this.txtpassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(326, 247);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(115, 45);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 358);
+            this.ClientSize = new System.Drawing.Size(765, 441);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtpassword);
             this.Controls.Add(this.lbpassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtuser);
             this.Controls.Add(this.lbUserName);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,6 +117,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lbpassword;
         private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
